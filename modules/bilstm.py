00f27e49092b09ef60da4865d469152a0725dd19
WangYudong <yudong.wang@akane.waseda.jp>
2020-12-01 13:39:10 +0900

    [add] torch modules

---
+++ modules/bilstm.py
@@ -0,0 +1,24 @@
+"""
+Definition of bi-directional LSTM model
+"""
+
+import torch
+import torch.nn as nn
+
+
+class BiLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, device):
+        super(BiLSTM, self).__init__()
+        self.device = device
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size*2, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(self.device)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
